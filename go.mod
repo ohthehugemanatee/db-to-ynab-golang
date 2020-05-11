9efbad36765c619d707721cff5a0092961fe5a28
Campbell Vertesi <campbell@vertesi.com>
2020-05-11 17:10:02 +0200

    Use a handler for the kickoff page

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ohthehugemanatee/db-to-ynab-golang
 
 go 1.14
+
+require golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
--- main.go
+++ main.go
@@ -1,10 +1,58 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/oauth2"
+)
 
 func main() {
-	fs := http.FileServer(http.Dir("public"))
-	http.Handle("/", fs)
-
-	http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<html><body>")
+	conf := getConf()
+	// Redirect user to consent page to ask for permission
+	// for the scopes specified above.
+	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
+	fmt.Fprintf(w, "Authenticate with <a href=\"%v\">Deutsche Bank</a>", url)
+}
+
+func getConf() *oauth2.Config {
+	conf := &oauth2.Config{
+		ClientID:     os.Getenv("DB_CLIENT_ID"),
+		ClientSecret: os.Getenv("DB_CLIENT_SECRET"),
+		Scopes:       []string{"read_transactions", "read_accounts", "read_credit_cards_list_with_details", "read_credit_card_transactions", "offline_access"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://simulator-api.db.com/gw/oidc/authorize",
+			TokenURL: "https://simulator-api.db.com/gw/oidc/token",
+		},
+	}
+	return conf
+}
+
+/**
+func receiver() {
+	ctx := context.Background()
+
+	// Use the authorization code that is pushed to the redirect
+	// URL. Exchange will do the handshake to retrieve the
+	// initial access token. The HTTP Client returned by
+	// conf.Client will refresh the token as necessary.
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatal(err)
+	}
+	tok, err := conf.Exchange(ctx, code)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf(tok.TokenType)
+
+}
+*/
