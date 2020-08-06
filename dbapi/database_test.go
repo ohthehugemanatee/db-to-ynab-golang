76e3db861540499e936304976f1da79a7cc286dd
Campbell Vertesi <campbell@vertesi.com>
2020-08-06 14:56:38 +0200

    Add setter

--- dbapi/database_test.go
+++ dbapi/database_test.go
@@ -1,20 +1,31 @@
 package dbapi
 
 import (
+	"bytes"
 	"encoding/json"
 	"testing"
 	"time"
 )
 
 func TestTokenFileStore(t *testing.T) {
-	t.Run("Get a token from the file store", func(t *testing.T) {
-		testToken := databaseRecord{
-			AccessToken:  "accessToken",
-			TokenType:    "tokenType",
-			RefreshToken: "refreshToken",
-			Expiry:       time.Now().String(),
-		}
+	testToken := databaseRecord{
+		AccessToken:  "accessToken",
+		TokenType:    "tokenType",
+		RefreshToken: "refreshToken",
+		Expiry:       time.Now().String(),
+	}
+	t.Run("Set a token record in a file store", func(t *testing.T) {
+		store := FileSystemTokenStore{}
+		store.setTokenRecord(testToken)
 
+		got := store.database
+		want, _ := json.Marshal(testToken)
+		if bytes.Compare(got, want) != 0 {
+			t.Errorf("Did not find the same token we set in the database. Got %s wanted %s", got, want)
+		}
+	})
+
+	t.Run("Get a token record from a file store", func(t *testing.T) {
 		json, _ := json.Marshal(testToken)
 		store := FileSystemTokenStore{json}
 
@@ -24,6 +35,5 @@ func TestTokenFileStore(t *testing.T) {
 		if got != want {
 			t.Errorf("Did not load the same token we set. Got %s wanted %s", got, want)
 		}
-
 	})
 }
