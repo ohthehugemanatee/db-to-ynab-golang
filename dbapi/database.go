7230fac76359a6f6405b7c4e3c03b15a8c54f2f5
Campbell Vertesi <campbell@vertesi.com>
2020-08-06 15:18:57 +0200

    Rename database to storage

--- dbapi/database.go
+++ dbapi/database.go
@@ -13,17 +13,17 @@ type databaseRecord struct {
 
 // FileSystemTokenStore is the token storage system.
 type FileSystemTokenStore struct {
-	database []byte
+	storage []byte
 }
 
 // GetTokenRecord gets a token record.
 func (f *FileSystemTokenStore) getTokenRecord() databaseRecord {
 	var record databaseRecord
-	json.Unmarshal(f.database, &record)
+	json.Unmarshal(f.storage, &record)
 	return record
 }
 
 func (f *FileSystemTokenStore) setTokenRecord(record databaseRecord) {
 	json, _ := json.Marshal(record)
-	f.database = json
+	f.storage = json
 }
