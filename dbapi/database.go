c949e72868a7020e5cdf34dde4d4490a292669d2
Campbell Vertesi <campbell@vertesi.com>
2020-08-10 21:12:10 +0200

    Cleanup whitespace

--- dbapi/database.go
+++ dbapi/database.go
@@ -116,17 +116,14 @@ func (f FileSystemTokenStore) encrypt(plaintext []byte, key []byte) ([]byte, err
 	if err != nil {
 		return nil, err
 	}
-
 	gcm, err := cipher.NewGCM(c)
 	if err != nil {
 		return nil, err
 	}
-
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
 		return nil, err
 	}
-
 	return gcm.Seal(nonce, nonce, plaintext, nil), nil
 }
 
@@ -135,17 +132,14 @@ func (f FileSystemTokenStore) decrypt(ciphertext []byte, key []byte) ([]byte, er
 	if err != nil {
 		return nil, err
 	}
-
 	gcm, err := cipher.NewGCM(c)
 	if err != nil {
 		return nil, err
 	}
-
 	nonceSize := gcm.NonceSize()
 	if len(ciphertext) < nonceSize {
 		return nil, errors.New("ciphertext too short")
 	}
-
 	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
 	return gcm.Open(nil, nonce, ciphertext, nil)
 }
