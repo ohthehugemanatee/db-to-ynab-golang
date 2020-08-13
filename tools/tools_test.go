b09c2cd93cab4cd64d726d6fbb171fa7b430f291
Campbell Vertesi <campbell@vertesi.com>
2020-08-13 13:01:16 +0200

    test log buffer

--- tools/tools_test.go
+++ tools/tools_test.go
@@ -1,6 +1,7 @@
 package tools
 
 import (
+	"log"
 	"testing"
 )
 
@@ -25,4 +26,85 @@ func TestConvertToMilliunits(t *testing.T) {
 	if float64(got) != expect {
 		t.Errorf("Milliunits conversion was incorrect. Expected %f got %d", expect, got)
 	}
+}
+
+func TestTestLogBuffer(t *testing.T) {
+	line1 := "One is the loneliest number"
+	line2 := "It takes two to tango"
+	line3 := "Three's a crowd"
+	t.Run("Identical single lines should pass", func(t *testing.T) {
+		logBuffer := CreateAndActivateEmptyTestLogBuffer()
+		logBuffer.ExpectLog(line1)
+		log.Print(line1)
+		newT := testing.T{}
+		logBuffer.TestLogValues(&newT)
+		if newT.Failed() {
+			t.Error("Identical log lines recorded a test failure")
+		}
+	})
+	t.Run("Different single lines should fail", func(t *testing.T) {
+		logBuffer := CreateAndActivateEmptyTestLogBuffer()
+		logBuffer.ExpectLog(line1)
+		log.Print(line2)
+		newT := testing.T{}
+		logBuffer.TestLogValues(&newT)
+		if !newT.Failed() {
+			t.Error("Different log lines did not result in test failure")
+		}
+	})
+	t.Run("Identical multiple lines should pass", func(t *testing.T) {
+		logBuffer := CreateAndActivateEmptyTestLogBuffer()
+		logBuffer.ExpectLog(line1)
+		logBuffer.ExpectLog(line2)
+		logBuffer.ExpectLog(line3)
+		log.Print(line1)
+		log.Print(line2)
+		log.Print(line3)
+		newT := testing.T{}
+		logBuffer.TestLogValues(&newT)
+		if newT.Failed() {
+			t.Error("Identical multiple log lines recorded as a test failure")
+		}
+	})
+	t.Run("Missing log lines should fail", func(t *testing.T) {
+		logBuffer := CreateAndActivateEmptyTestLogBuffer()
+		logBuffer.ExpectLog(line1)
+		logBuffer.ExpectLog(line2)
+		logBuffer.ExpectLog(line3)
+		log.Print(line3)
+		log.Print(line1)
+		newT := testing.T{}
+		logBuffer.TestLogValues(&newT)
+		if !newT.Failed() {
+			t.Error("Missing log lines did not result in test failure")
+		}
+	})
+	t.Run("Extra unexepcted log lines should fail", func(t *testing.T) {
+		logBuffer := CreateAndActivateEmptyTestLogBuffer()
+		logBuffer.ExpectLog(line1)
+		logBuffer.ExpectLog(line3)
+		log.Print(line3)
+		log.Print(line2)
+		log.Print(line1)
+		newT := testing.T{}
+		logBuffer.TestLogValues(&newT)
+		if !newT.Failed() {
+			t.Error("Extra unexpected log lines did not result in test failure")
+		}
+	})
+	t.Run("Identical multiple lines in different order should fail", func(t *testing.T) {
+		logBuffer := CreateAndActivateEmptyTestLogBuffer()
+		logBuffer.ExpectLog(line1)
+		logBuffer.ExpectLog(line2)
+		logBuffer.ExpectLog(line3)
+		log.Print(line3)
+		log.Print(line2)
+		log.Print(line1)
+		newT := testing.T{}
+		logBuffer.TestLogValues(&newT)
+		if !newT.Failed() {
+			t.Error("Identical multiple log lines in the wrong order did not result in test failure")
+		}
+	})
+
 }
